a3e99bbe9d6b03b47ff76e71a75d6ce93f0b42ee
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-16 01:47:09 +0900

    fix(component): change floating button animation speed & toggle button layout

--- src/animations/button.ts
+++ src/animations/button.ts
@@ -3,7 +3,7 @@ import { Variants } from 'framer-motion';
 export const FloatingButtonVariant: Variants = {
   enter: { translateY: 0, translateX: 0 },
   center: (delay) => {
-    return { translateY: -10 * delay, translateX: 0, transition: { duration: delay * 0.15 } };
+    return { translateY: -10 * delay, translateX: 0, transition: { duration: delay * 0.1 } };
   },
   exit: (delay) => {
     return {
