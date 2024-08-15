30e469c51f67b3f1f58b43c4d6c50330d5298a49
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-16 01:47:06 +0900

    chore(animation): Remove Page transition Effect

--- src/animations/page.ts
+++ src/animations/page.ts
@@ -1,6 +1,16 @@
 import { Variants } from 'framer-motion';
 
 export const pageVars: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  enter: { opacity: 1, x: 0, y: 0, transition: { duration: 0.5 } },
+  exit: {
+    opacity: 0,
+  },
+};
+
+export const appLikePageVars: Variants = {
   hidden: (direction: number) => {
     return {
       x: direction > 0 ? 1000 : -1000,
