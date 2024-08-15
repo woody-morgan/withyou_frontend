c02b4a0c01bf38cbd83e2d57ce322463be44bf5e
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-16 01:47:10 +0900

    feat(): responsive view on album page

--- src/pages/album.tsx
+++ src/pages/album.tsx
@@ -2,42 +2,29 @@ import { PageSEO } from '@src/components/analytics/SEO';
 import { withAuthCSR, withAuthSSR } from '@src/components/hoc';
 import { PageLayout } from '@src/components/layout';
 import AlbumCommonHeader from '@src/components/template/AlbumPage/AlbumCommonHeader';
-import { apiGetFamilyPhotos } from '@src/core/api/apiAlbum';
+import { ImageWrapper } from '@src/components/ui/atom';
+import { ApiFamilyPhotosResponse, apiGetFamilyPhotos } from '@src/core/api/apiAlbum';
 import siteMetadata from '@src/core/config/siteMetadata';
-import { getRandomImageRadio } from '@src/utils/imageUtil';
+import cx from 'classnames';
 import { NextPage } from 'next';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import Gallery from 'react-photo-gallery';
-
-interface IinitialPhoto {
-  src: string;
-  width: number;
-  height: number;
-}
 
 interface AlbumPageProps {
-  initialPhotos: IinitialPhoto[];
+  initialPhotos: ApiFamilyPhotosResponse;
 }
 
 export const getServerSideProps = withAuthSSR(async () => {
   const photoInfos = await apiGetFamilyPhotos();
 
-  const ret = photoInfos.media.map((photoInfo) => {
-    const rand = getRandomImageRadio();
-    return {
-      src: photoInfo.url,
-      ...rand,
-    };
-  });
-
   return {
     props: {
-      initialPhotos: ret,
+      initialPhotos: photoInfos,
     },
   };
 });
 
-const AlbumPage: NextPage<AlbumPageProps> = ({ initialPhotos }) => {
+const AlbumPage: NextPage<AlbumPageProps> = ({ initialPhotos: { media } }) => {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -50,8 +37,22 @@ const AlbumPage: NextPage<AlbumPageProps> = ({ initialPhotos }) => {
   return (
     <PageLayout showNavigation fixedHeight headerContent={<AlbumCommonHeader />}>
       <PageSEO title={siteMetadata.title + ' Album Page'} description={'album page'} />
-      <div className="w-full h-full overflow-scroll py-5">
-        {mounted && initialPhotos.length > 0 && <Gallery photos={initialPhotos} />}
+      <div className="w-full h-full overflow-scroll py-5 flex flex-wrap">
+        {mounted &&
+          media.length > 0 &&
+          media.map((photo, idx) => (
+            <Link key={`album-photo-${idx}`} href={`/diary/${photo.diaryId}`}>
+              <a
+                className={cx(
+                  'relative h-[200px]',
+                  'basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5',
+                  'border-[1px] border-solid border-gray-200/50'
+                )}
+              >
+                <ImageWrapper src={photo.url} layout="fill" objectFit="cover" />
+              </a>
+            </Link>
+          ))}
       </div>
     </PageLayout>
   );
